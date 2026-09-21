c4c10d4a8614d8aa0fbf70e52a85fe345b529433
Nandini kante <nandinikante896@gmail.com>
2026-09-21 22:49:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,10 +35,13 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
 MODEL_PATH = BASE_DIR / "models" / "phishing_ann_model.keras"
 SCALER_PATH = BASE_DIR / "models" / "scaler.pkl"
 FEATURE_PATH = BASE_DIR / "feature_names.json"
-
 # ===============================================
 # ============================================================
 # LOAD MODEL
